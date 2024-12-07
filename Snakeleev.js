068bba0f325a4a0561d30ee5a1ba4809cf4e9347
Pietro Galizia <159483860+PietroGalizia@users.noreply.github.com>
2024-12-08 00:39:06 +0100

    Update Snakeleev.js

--- Snakeleev.js
+++ Snakeleev.js
@@ -380,7 +380,7 @@ function handleEating() {
     if (head.x === food.x && head.y === food.y) {
         if (diets[selectedDiet] && diets[selectedDiet].includes(foodElement)) {
             // Se l'elemento appartiene alla dieta
-            const scoreIncrement = Math.round((100 * (rangeValue - validDietElementsCount) / rangeValue));
+            const scoreIncrement = 100 * (rangeValue - validDietElementsCount) / rangeValue;
             score += scoreIncrement;
 
             scoreText = {
@@ -400,7 +400,7 @@ function handleEating() {
             expandFoodEffect(food.x, food.y);
         } else {
             // Se l'elemento non appartiene alla dieta
-            const scoreDecrement = Math.round((100 * validDietElementsCount / rangeValue));
+            const scoreDecrement = 100 * validDietElementsCount / rangeValue;
             score -= scoreDecrement;
 
             scoreTextNo = {
