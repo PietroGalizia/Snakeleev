90294f9ce4bbb7d51acb2eba253ade002f4ab248
Pietro Galizia <159483860+PietroGalizia@users.noreply.github.com>
2024-12-09 22:46:20 +0100

    Update Snakeleev.js

--- Snakeleev.js
+++ Snakeleev.js
@@ -312,15 +312,14 @@ function createInfoRectNo(element, x, y) {
 function updateScore(newScore) {
     const scoreBoard = document.getElementById('scoreBoard');
     scoreBoard.style.color = "fff";
-    scoreBoard.style.padding = "0px";
+    scoreBoard.style.padding = "5px";
     scoreBoard.style.border = "2px solid #78b3e0";
     scoreBoard.style.borderRadius = "0px";
     scoreBoard.style.fontFamily = "Arial, sans-serif";
     scoreBoard.style.backgroundColor = "rgb(0, 47, 95)";
     scoreBoard.style.textAlign = "center";
     scoreBoard.style.margin = "0px auto";
-    const canvasWidth = canvas.offsetWidth;
-    scoreBoard.style.width = `${canvasWidth}px`;
+    scoreBoard.style.width = CANVAS_WIDTH;
 
     scoreBoard.innerHTML = `
         <div style="font-size: 1.2em; font-weight: bold; margin-bottom: 5px; color: #f79d27#8f7dcf;">
