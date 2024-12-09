60182ca8d1268abfb101f6cc13118d7f214fc63a
Pietro Galizia <159483860+PietroGalizia@users.noreply.github.com>
2024-12-09 14:04:43 +0100

    Update Snakeleev.js

--- Snakeleev.js
+++ Snakeleev.js
@@ -462,7 +462,7 @@ function updateGame(ctx) {
         const expandInterval = setInterval(() => {
             size += 2;
             ctx.clearRect(x - size / 2, y - size / 2, size, size);
-            ctx.fillStyle = "rgb(120, 179, 224)";
+            ctx.fillStyle = "rgb(150, 1749, 33)";
             ctx.fillRect(x - size / 2, y - size / 2, size, size);
         }, 30);
         setTimeout(() => {
@@ -571,10 +571,6 @@ function updateGame(ctx) {
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
     ctx.fillText(foodElement, food.x + SIZE / 2, food.y + SIZE / 2);
-
-    // Disegna il numero atomico sotto il simbolo
-    //ctx.font = "12px Arial"; // Numero atomico più piccolo
-    //ctx.fillText(foodElementNumber, food.x + SIZE / 2, food.y + (2 * SIZE) / 3);
 
     // Draw game area border
     ctx.strokeStyle = "#83B7DE";
