17cc9a052d6e6d6d0c5c94e4da5a9613fb8480f7
Pietro Galizia <159483860+PietroGalizia@users.noreply.github.com>
2024-12-11 12:23:36 +0100

    Update Snakeleev.js

--- Snakeleev.js
+++ Snakeleev.js
@@ -604,7 +604,7 @@ function initializeHPBar() {
         const square = document.createElement('div');
         square.className = 'hpSquare';
         if (i < 3) square.style.backgroundColor = 'rgb(229, 26, 75)';
-        else if (i < 4) square.style.backgroundColor = 'rgb(247, 157, 39)';
+        else if (i < 5) square.style.backgroundColor = 'rgb(247, 157, 39)';
         else if (i < 7) square.style.backgroundColor = 'rgb(150, 174, 33)';
         else square.style.backgroundColor = 'rgb(65, 127, 69)';
         hpBarContainer.appendChild(square);
